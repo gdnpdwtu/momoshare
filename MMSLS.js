27eb937073411f04d4f44170ae7e0cd3f7ef3704
gdnpdwtu <40480319+gdnpdwtu@users.noreply.github.com>
2024-02-20 08:23:37 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=27304704&pid=6bf52266b5a98e33e6f486ccc9a0239f&tid=1289674d5613cfaa348be7983833d6ae",
+    "https://www.maimemo.com/share/page?uid=27304704&pid=77850b09aebb6d73de995df972ee2009&tid=9cfedde62be1ffb30ae988c98d83d4d4",
 
   ]
   /**
